cdeb11709be83da98292fdc47021a9fc9dcf5f5b
aksayed <amrkhaled286@gmail.com>
2025-06-17 15:12:41 +0300

    Add files via upload

---
+++ scripts/element.js
@@ -0,0 +1,12 @@
+export const toggleDarkMode = document.querySelector(".DarkThemeToggle");
+export const avtive = document.querySelector(".DarkThemeToggle__activate");
+export const deAvtive = document.querySelector(".DarkThemeToggle__deactivate");
+export const app = document.querySelector(".App");
+export const inputTask = document.querySelector(".TaskSearchBar__input");
+export const addTaskButton = document.querySelector(".TaskSearchBar__button");
+export const containerTask = document.querySelector(".TaskList__list");
+export const getDeleteIcons = () =>
+  document.querySelectorAll(".TaskList__deleteIcon");
+export const getCheckIcons = () =>
+  document.querySelectorAll(".TaskList__checkbox");
+export const toggleTaskListLink = document.querySelector(".TaskList__link");
---
+++ scripts/utils.js
@@ -0,0 +1,78 @@
+import { initTaskLisreners } from "./addEvent.js";
+import { app, containerTask, inputTask } from "./element.js";
+import { renderTaskList } from "./renderTaskList.js";
+
+export const renderEmptyStates = () => {
+  containerTask.innerHTML = `
+    <li class="EmptyList">
+      <img src="/assets/icon-empty.svg" alt="empty-icon" class="EmptyList__img" />
+      <p>قائمة المهام فارغة</p>
+    </li>
+  `;
+};
+
+export const initTaskList = (tasks) => {
+  if (tasks?.length) {
+    renderTaskList(tasks);
+    initTaskLisreners();
+  } else {
+    renderEmptyStates();
+  }
+};
+
+export const addElements = (e) => {
+  e.preventDefault();
+  if (!inputTask.value) return;
+
+  const task = {
+    value: inputTask.value,
+    isCompleted: false,
+  };
+
+  const tasks = fetchData("tasks") || [];
+  tasks.push(task);
+  saveToDB("tasks", tasks);
+
+  initTaskList(tasks);
+};
+
+export const initDataOnStartUp = () => {
+  fetchData("darkModeFlag") && darkMode();
+
+  initTaskList(fetchData("tasks"));
+};
+
+export const darkMode = () => {
+  app.classList.toggle("App--isDark");
+  saveToDB("darkModeFlag", app.classList.contains("App--isDark"));
+};
+
+export const fetchData = (key) => {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+};
+
+export const saveToDB = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const deleteTask = (e, index) => {
+  const answer = confirm("هل أنت متأكد من حذف المهمة؟");
+  if (answer === false) return;
+
+  const tasks = fetchData("tasks");
+
+  tasks.splice(index, 1);
+  saveToDB("tasks", tasks);
+  initTaskList(tasks);
+};
+
+export const toggleTask = (e, index) => {
+  const tasks = fetchData("tasks");
+
+  e.currentTarget.parentElement.classList.toggle(
+    "TaskList__taskContent--isActive"
+  );
+  tasks[index].isCompleted = !tasks[index].isCompleted;
+  saveToDB("tasks", tasks);
+};
